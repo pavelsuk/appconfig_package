5fee82810741a2bccbc52962f3ffa557e5312649
pavelsuk <pavelsuk@users.noreply.github.com>
2022-06-25 16:24:00 +0200

    let's delete previous build files

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 REM call bumpver update --patch --dry
 REM call bumpver update --patch
 call bumpver update
+del dist\*.* /q
 call python -m build
 call python -m pip install -e .
 REM call twine upload -r testpypi dist/*
